1b22d96c36138b92b73117bd1363829c33c5b0e1
OmSisode <omsisode1212@gmail.com>
2025-09-22 13:00:23 +0000

    container config done

--- 01_Basic/variable.js
+++ 01_Basic/variable.js
@@ -1,6 +1,7 @@
 let AccountId = 12234;
 let AccountPass = 122;
 let AccountName = "om";
+accountBalance=10000;
 {
-    console.table([AccountId,AccountPass,AccountName]);
+    console.table([AccountId,AccountPass,AccountName,accountBalance]);
 }
